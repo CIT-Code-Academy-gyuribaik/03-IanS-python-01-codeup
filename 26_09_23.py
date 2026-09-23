623a4afac6707eda709c6eaea94879af17143e2b
Ian Song <ian.song1@seoulacademy.org>
2026-09-23 16:00:34 +0900

    Update 26_09_23.py

--- 26_09_23.py
+++ 26_09_23.py
@@ -1,5 +1,15 @@
 # 6078 : [Basic-Comprehensive] Repeat printing until a desired character is entered (py)
 a="y"
 while a!="q":
-  a=input()
+  a=ihttps://github.com/CIT-Code-Academy-gyuribaik/03-IanS-python-01-codeup/pullsnput()
   print(a)
+
+
+#6079 : [Basic-Comprehensive] Until when should I add?(py)
+a=int(input()) 
+sum=0
+i=1
+while sum<a:
+ sum=sum+i
+ i=i+1
+print(i-1)
